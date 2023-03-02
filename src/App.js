fd74c8551c9103bbc3d0f1ce55ef2afcdc04f5bc
VladITGod <vorobiov.v90@gmail.com>
2023-03-02 17:03:50 +0200

    init project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Collapse from "./components/Collapse";
 
 function App() {
+  const text = 'collapse me';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Collapse text={text} opened={false} />
     </div>
   );
 }
---
+++ src/components/Collapse.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import cn from 'classnames'
+
+class Collapse extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+
+    render () {
+
+
+
+    }
+}
+
+export default Collapse

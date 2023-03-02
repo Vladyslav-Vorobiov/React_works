d8018fc91dc9b76d36a63afa77a5ac8081fca735
VladITGod <vorobiov.v90@gmail.com>
2023-03-02 16:24:53 +0200

    Carousel component imported

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import Carousel from "./components/Carousel";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const pictures = [
+        {
+            theme: 'nature',
+            src: 'https://mcdn.wallpapersafari.com/medium/27/23/Itye3W.jpg'
+        },
+        {
+            theme: 'nature',
+            src: 'https://mcdn.wallpapersafari.com/medium/64/66/s0DCKZ.jpg'
+        },
+        {
+            theme: 'universe',
+            src: 'https://mcdn.wallpapersafari.com/medium/53/36/funlTU.jpg'
+        },
+        {
+            theme: 'city',
+            src: 'https://www.ece.cmu.edu/images/stock-images/smart-cities-500x300.png'
+        },
+        {
+            theme: 'food',
+            src: 'https://www.nuffieldhealth.com/local/39/19/b70d17f7427caba2b9ef7a1a50a8/c-users-rennisona-desktop-turmeric-500x300.jpg'
+        }
+    ]
+
+    return (
+        <div
+            className="App">
+            <Carousel images={pictures}/>
+        </div>
+    );
 }
 
 export default App;

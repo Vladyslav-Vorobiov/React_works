8a5420a688017a7f9d4fbc231061627a98ba515d
VladITGod <vorobiov.v90@gmail.com>
2023-03-02 18:26:41 +0200

    Collapse component created

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -5,16 +5,45 @@ class Collapse extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            isOpened: this.props.opened
+        };
+    };
 
-        }
+    handleClick = (e) => {
+        e.preventDefault();
+        this.setState({isOpened: this.state.isOpened ? false : true})
+    };
+
+    render() {
+        const {text} = this.props;
+        const isCollapsed = cn("collapse", {
+            show: this.state.isOpened === true,
+        });
+
+        return (
+            <div>
+                <p>
+                    <a className="btn btn-primary"
+                       data-bs-toggle="collapse"
+                       href="#"
+                       role="button"
+                       aria-expanded={this.state.isOpened}
+                       onClick={this.handleClick}> Link with href
+                    </a>
+                </p>
+                <div className={isCollapsed}>
+                    <div className="card card-body">
+                        {text}
+                    </div>
+                </div>
+            </div>
+        )
     }
+}
 
-
-    render () {
-
-
-
-    }
+Collapse.defaultProps = {
+    text: "Hello! Nice to meet you!",
+    opened: true
 }
 
 export default Collapse

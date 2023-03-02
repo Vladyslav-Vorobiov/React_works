e91c6043546b15c7bcb909dd3902d8fcaf636261
VladITGod <vorobiov.v90@gmail.com>
2023-03-02 16:54:04 +0200

    a few spaces deleted

--- src/App.js
+++ src/App.js
@@ -26,8 +26,7 @@ function App() {
     ]
 
     return (
-        <div
-            className="App">
+        <div className="App">
             <Carousel images={pictures}/>
         </div>
     );
